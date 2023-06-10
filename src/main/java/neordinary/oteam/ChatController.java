f82a2b3faa15baec00ec06ad7884232f6cfbbb37
daeunkwak <misodaeun3@naver.com>
2023-06-10 20:54:34 +0900

    feat: api path 추가

--- src/main/java/neordinary/oteam/ChatController.java
+++ src/main/java/neordinary/oteam/ChatController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
+@RequestMapping("/api")
 public class ChatController {
 
     private final OpenAIService openAIService;
