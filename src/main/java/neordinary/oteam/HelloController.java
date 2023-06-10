ee0c3f6d4a62f1ed8b2f38ea98dc284fa592f98c
hojeong2747 <hojeong2747@gmail.com>
2023-06-10 17:17:29 +0900

    add: HelloController

---
+++ src/main/java/neordinary/oteam/HelloController.java
@@ -0,0 +1,22 @@
+package neordinary.oteam;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/sample")
+    public String sample() {
+        return "Sample!!!!!!!!";
+    }
+
+    @GetMapping("/api/sample")
+    public String sampleV2() {
+        return "Sample!!!!!!!!";
+    }
+
+}
