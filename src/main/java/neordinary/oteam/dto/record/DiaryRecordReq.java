2119557792bdcace11e3c6af2ff27bd59510089d
hojeong2747 <hojeong2747@gmail.com>
2023-06-11 05:26:07 +0900

    fix: record swagger api

--- src/main/java/neordinary/oteam/dto/record/DiaryRecordReq.java
+++ src/main/java/neordinary/oteam/dto/record/DiaryRecordReq.java
@@ -1,5 +1,7 @@
 package neordinary.oteam.dto.record;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +9,8 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel(value = "다이어리 레코드 생성 요청 객체")
 public class DiaryRecordReq {
+    @ApiModelProperty(value = "유저 이름")
     String name;
 }
